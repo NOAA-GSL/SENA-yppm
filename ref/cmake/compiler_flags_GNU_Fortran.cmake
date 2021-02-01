7bf543c20f0b0b4aade8f048d2216c8f4cfe8e87
LyndStringer <Lynd.Stringer@noaa.gov>
2021-02-01 15:31:41 +0000

    Fix for issues #28 and #29. For issue and bug #28, the GNU and Intel debug compilations are not apples-to-apples.  The GNU debug compilation is at -O1 and the Intel debug compilation is at -O0.  The compiler creates code motion at higher and higher optimizaiton levels.  Code motion means that the assembly instructions are moved around so the CPU can execute the instructions faster.  To help debug a program, it is helpful to group the assembly instructions with their source code lines.  Code motion ungroups the instructions, which makes it harder to debug. So -O0 is the best optimization for debug.  So we are setting the GNU debug level to -O0.  This makes it apples-to-apples with the Intel debug level and helps with debugging.
    For issue and bug #29, the GNU optimization is -O2 at the release level.
    Unfortunately, the GNU compiler does not vectorize at -O2.  So we
    are changing the optimization level to -O3, which turns on
    vectorization.  This makes it apples-to-apples with the Intel
    release optimization level.

--- ref/cmake/compiler_flags_GNU_Fortran.cmake
+++ ref/cmake/compiler_flags_GNU_Fortran.cmake
@@ -1,16 +1,16 @@
 ####################################################################
 # COMMON FLAGS
 ####################################################################
-set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fbacktrace -ffloat-store -fcray-pointer -fno-unsafe-math-optimizations -frounding-math -ffp-contract=off")
+set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -fbacktrace -ffloat-store -fcray-pointer -fno-unsafe-math-optimizations -frounding-math -ffp-contract=off")
 
 ####################################################################
 # RELEASE FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_RELEASE "-O2 -march=native -funroll-all-loops -finline-functions")
+set( CMAKE_Fortran_FLAGS_RELEASE "-O3 -march=native -funroll-all-loops -finline-functions")
 
 ####################################################################
 # DEBUG FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -O1 -g -fcheck=bounds -ffpe-trap=invalid,zero,overflow" )
+set( CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -O0 -fcheck=bounds -ffpe-trap=invalid,zero,overflow" )
